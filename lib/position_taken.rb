019907604dd621a93352669ed95b8bf064d6145c
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 05:43:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  #return false if board[index] = ""
   if board[index] == "X" || board[index] == "O"
     return true 
   else
